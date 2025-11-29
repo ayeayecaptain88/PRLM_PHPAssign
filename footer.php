e63ecda68e95aae235fe98bcc264100c5831fee6
ayeayecaptain88 <wg.ayennesoc@gmail.com>
2025-11-29 21:41:21 +0800

    Delete footer.php

--- footer.php
+++
@@ -1,9 +0,0 @@
-</div>
-
-<footer style="text-align:center; padding:15px; margin-top:30px; 
-background:#b7e3ea; color:#03484d; border-top:2px solid #8fc8d1;">
-    MedExpress © 2025 - Pharmacy Supply System  | Created for Academic Purposes: Ayenne Wage
-</footer>
-
-</body>
-</html>
